e1c1b1469bdc2c4a54854b34cc1b8f7ff418943f
embrie-delarosa <delarosaembriea@gmail.com>
2026-02-19 02:53:38 +0800

    Added Signup Layout

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,17 +1,18 @@
-// 
-
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
