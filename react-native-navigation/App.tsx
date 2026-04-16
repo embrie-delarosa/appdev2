2bafa3b2df64052a37adb3870774d52d05c8c722
embrie-delarosa <delarosaembriea@gmail.com>
2026-04-17 02:07:49 +0800

    Done with navigate to a screen multiple times

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -21,6 +21,7 @@ function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button onPress={() => navigation.push('Details')}>
     </View>
   );
 }
