726a400c2da76ab5b0eecc1b413ad873b079b9eb
embrie-delarosa <delarosaembriea@gmail.com>
2026-04-17 02:11:53 +0800

    Done with going back

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -18,13 +18,23 @@ function HomeScreen() {
 }
 
 function DetailsScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
       <Button onPress={() => navigation.push('Details')}>
+        Go to Details... again
+      </Button>
+      <Button onPress={() => navigation.goBack()}>Go back</Button>
+      <Button onPress={() => navigation.popTo('Home')}>Go to Home</Button>
+      <Button onPress={() => navigation.popToTop()}>
+        Go back to first screen in stack
+      </Button>
     </View>
   );
 }
+
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
